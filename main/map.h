a4af9f499eec41a0f06d28850ce495c935d608b9
ypc2e55orj <ypc2e55orj@gmail.com>
2024-02-15 00:56:30 +0900

    迷路保持形式を変更

--- main/map.h
+++ main/map.h
@@ -21,133 +21,61 @@ class Map {
     DIRECTION_SOUTH = 0x02,
     DIRECTION_WEST = 0x03,
   };
+
+  // 各方位での壁の状態を示す
+  union Walls {
+    struct {
+      bool north : 1;
+      bool east : 1;
+      bool south : 1;
+      bool west : 1;
+      uint8_t _padding : 4;
+    } exist;
+    struct {
+      uint8_t _padding : 4;
+      bool north : 1;
+      bool east : 1;
+      bool south : 1;
+      bool west : 1;
+    } stepped;
+    struct {
+      uint8_t exist : 4;
+      uint8_t stepped : 4;
+    } byte;
+  };
+
   // 迷路での座標
   struct Coord {
     int x;
     int y;
   };
-  // 壁の状態を定義
-  // 探索走行のときは未探索も含めて確認する
-  // 最短走行のときは完全に壁がないことを確認する
-  enum Wall : uint8_t {
-    WALL_NOT_EXISTS = 0x00,
-    WALL_EXISTS = 0x01,
-    WALL_NOT_VISITED = 0x02,
-    WALL_VIRTUAL = 0x03,
-  };
-  // 各方位での壁の状態を示す
-  union Walls {
-    struct {
-      Wall north : 2;
-      Wall east : 2;
-      Wall south : 2;
-      Wall west : 2;
-    };
-    uint8_t byte;
-  };
 
-  /**
-   * コンストラクタ / デストラクタ
-   */
+  // 標準出力時の壁の色を定義
+  static constexpr auto WALL_COLOR_NOT_EXISTS = "\x1b[37m";
+  static constexpr auto WALL_COLOR_EXISTS = "\x1b[37m";
+  static constexpr auto WALL_COLOR_UNKNOWN = "\x1b[31m";
+  static constexpr auto WALL_COLOR_RESET = "\x1b[0m";
+
+  // コンストラクタ
   explicit Map(const int (&goal_xs)[MAZE_GOAL_SIZE_X],
-               const int (&goal_ys)[MAZE_GOAL_SIZE_Y])
-      : steps_(), walls_(), dir_(), pos_() {
-    initWalls();
-    initStepsToGoal(goal_xs, goal_ys);
-  }
-  ~Map() = default;
+               const int (&goal_ys)[MAZE_GOAL_SIZE_Y]);
+  // デストラクタ
+  ~Map();
 
   // 壁情報を初期化
-  void initWalls() {
-    // 壁を削除
-    for (auto &row : walls_) {
-      for (auto &wall : row) {
-        wall.north = WALL_NOT_VISITED;
-        wall.east = WALL_NOT_VISITED;
-        wall.south = WALL_NOT_VISITED;
-        wall.west = WALL_NOT_VISITED;
-      }
-    }
-    // 外周の壁を設定(横)
-    for (auto x = 0; x < MAZE_SIZE_X; x++) {
-      walls_[0][x].south = WALL_EXISTS;
-      walls_[MAZE_SIZE_Y - 1][x].north = WALL_EXISTS;
-    }
-    // 外周の壁を設定(横)
-    for (auto y = 0; y < MAZE_SIZE_Y; y++) {
-      walls_[y][0].west = WALL_EXISTS;
-      walls_[y][MAZE_SIZE_X - 1].east = WALL_EXISTS;
-    }
-    // スタート座標の右壁
-    walls_[0][0].east = WALL_EXISTS;
-    walls_[0][1].west = WALL_EXISTS;
-  }
+  void initWalls();
 
   // スタートまでの歩数マップを初期化
-  void initStepsToStart() {
-    initSteps();
-    steps_[0][0] = 0;
-    updateQueue_.push({0, 0});
-  }
+  void initStepsToStart();
   // ゴールまでの歩数マップを初期化
   void initStepsToGoal(const int (&goal_xs)[MAZE_GOAL_SIZE_X],
-                       const int (&goal_ys)[MAZE_GOAL_SIZE_Y]) {
-    initSteps();
-    // ゴール座標の歩数を最小値に設定
-    for (const auto &y : goal_ys) {
-      for (const auto &x : goal_xs) {
-        steps_[x][y] = 0;
-        updateQueue_.push({x, y});
-      }
-    }
-  }
+                       const int (&goal_ys)[MAZE_GOAL_SIZE_Y]);
+
   // 歩数を作成
-  void makeSteps(bool shortest) {
-    const auto mask = shortest ? WALL_VIRTUAL : WALL_EXISTS;
-
-    while (!updateQueue_.empty()) {
-      // 先頭を参照
-      const auto &coord = updateQueue_.front();
-
-      /**
-       * 隣接区画を確認
-       * 探索のときは未探索も壁なしとして扱う
-       * 最短のときはすべて壁なしの場合のみ
-       */
-      const auto &step = steps_[coord.y][coord.x];
-      const auto &wall = walls_[coord.y][coord.x];
-      // 北
-      if (coord.y + 1 < MAZE_SIZE_Y && (wall.north & mask) == 0 &&
-          steps_[coord.y + 1][coord.x] == 255) {
-        steps_[coord.y + 1][coord.x] = step + 1;
-        updateQueue_.push({coord.x, coord.y + 1});
-      }
-      // 東
-      if (coord.x + 1 < MAZE_SIZE_X && (wall.east & mask) == 0 &&
-          steps_[coord.y][coord.x + 1] == 255) {
-        steps_[coord.y][coord.x + 1] = step + 1;
-        updateQueue_.push({coord.x + 1, coord.y});
-      }
-      // 南
-      if (coord.y - 1 > -1 && (wall.south & mask) == 0 &&
-          steps_[coord.y - 1][coord.x] == 255) {
-        steps_[coord.y - 1][coord.x] = step + 1;
-        updateQueue_.push({coord.x, coord.y - 1});
-      }
-      // 西
-      if (coord.x - 1 > -1 && (wall.west & mask) == 0 &&
-          steps_[coord.y][coord.x - 1] == 255) {
-        steps_[coord.y][coord.x - 1] = step + 1;
-        updateQueue_.push({coord.x - 1, coord.y});
-      }
-
-      // 削除
-      updateQueue_.pop();
-    }
-  }
+  void makeSteps(bool shortest);
 
   // 自身の位置を取得する
-  const Map::Coord &getPos() { return pos_; }
+  const Coord &getPos() { return pos_; }
   // 自身の位置を設定する
   void setPos(int x, int y) {
     pos_.x = x;
@@ -170,6 +98,9 @@ class Map {
         break;
     }
   }
+  // 自身の方位を回転させる
+  void rotateDir() { dir_ = static_cast<Direction>((dir_ + 2) & 0x03); }
+
   // 自身がゴールしているか取得する
   [[nodiscard]] bool inGoal(const int (&goal_xs)[MAZE_GOAL_SIZE_X],
                             const int (&goal_ys)[MAZE_GOAL_SIZE_Y]) const {
@@ -185,243 +116,19 @@ class Map {
 
   // 壁を設定する
   void setWall(int x, int y, bool frontLeft, bool left, bool right,
-               bool frontRight) {
-    Map::Walls walls{};
-    switch (dir_) {
-      case DIRECTION_NORTH:
-        walls.north =
-            frontRight || frontLeft ? Map::WALL_EXISTS : Map::WALL_NOT_EXISTS;
-        walls.east = right ? Map::WALL_EXISTS : Map::WALL_NOT_EXISTS;
-        walls.west = left ? Map::WALL_EXISTS : Map::WALL_NOT_EXISTS;
-        walls.south = Map::WALL_NOT_EXISTS;
-        break;
-      case DIRECTION_EAST:
-        walls.east =
-            frontRight || frontLeft ? Map::WALL_EXISTS : Map::WALL_NOT_EXISTS;
-        walls.south = right ? Map::WALL_EXISTS : Map::WALL_NOT_EXISTS;
-        walls.north = left ? Map::WALL_EXISTS : Map::WALL_NOT_EXISTS;
-        walls.west = Map::WALL_NOT_EXISTS;
-        break;
-      case DIRECTION_SOUTH:
-        walls.south =
-            frontRight || frontLeft ? Map::WALL_EXISTS : Map::WALL_NOT_EXISTS;
-        walls.west = right ? Map::WALL_EXISTS : Map::WALL_NOT_EXISTS;
-        walls.east = left ? Map::WALL_EXISTS : Map::WALL_NOT_EXISTS;
-        walls.north = Map::WALL_NOT_EXISTS;
-        break;
-      case DIRECTION_WEST:
-        walls.west =
-            frontRight || frontLeft ? Map::WALL_EXISTS : Map::WALL_NOT_EXISTS;
-        walls.north = right ? Map::WALL_EXISTS : Map::WALL_NOT_EXISTS;
-        walls.south = left ? Map::WALL_EXISTS : Map::WALL_NOT_EXISTS;
-        walls.east = Map::WALL_NOT_EXISTS;
-        break;
-    }
-    setWall(x, y, walls);
-  }
-  void setWall(int x, int y, Walls &walls) {
-    // 壁を設定
-    walls_[y][x].byte = walls.byte;
-    // 隣接区画の壁も更新
-    if (y + 1 < MAZE_SIZE_Y) {
-      // 北 - 南
-      walls_[y + 1][x].south = walls.north;
-    }
-    if (x + 1 < MAZE_SIZE_X) {
-      // 東 - 西
-      walls_[y][x + 1].west = walls.east;
-    }
-    if (y - 1 > -1) {
-      // 南 - 北
-      walls_[y - 1][x].north = walls.south;
-    }
-    if (x - 1 > -1) {
-      // 西 - 東
-      walls_[y][x - 1].east = walls.west;
-    }
-  }
+               bool frontRight);
+  void setWall(int x, int y, Walls walls);
 
   // 次に進む方向を取得する
-  Direction getNextDir(bool shortest) {
-    const auto mask = shortest ? WALL_VIRTUAL : WALL_EXISTS;
-    Direction dir{};
-    uint8_t minStep = 255;
-    int priority = 0;
-
-    // 北
-    if ((walls_[pos_.y][pos_.x].north & mask) == WALL_NOT_EXISTS &&
-        pos_.y + 1 < MAZE_SIZE_Y) {
-      int pri = getPriority(pos_.x, pos_.y + 1, DIRECTION_NORTH);
-      auto step = steps_[pos_.y + 1][pos_.x];
-      if (step < minStep) {
-        // 歩数が少ない方を採用
-        minStep = step;
-        dir = DIRECTION_NORTH;
-        priority = pri;
-      } else if (step == minStep && priority < pri) {
-        // 歩数が同じなら、優先度で判断
-        dir = DIRECTION_NORTH;
-        priority = pri;
-      }
-    }
-    // 東
-    if ((walls_[pos_.y][pos_.x].east & mask) == WALL_NOT_EXISTS &&
-        pos_.x + 1 < MAZE_SIZE_X) {
-      int pri = getPriority(pos_.x + 1, pos_.y, DIRECTION_EAST);
-      auto step = steps_[pos_.y][pos_.x + 1];
-      if (step < minStep) {
-        minStep = step;
-        dir = DIRECTION_EAST;
-        priority = pri;
-      } else if (step == minStep && priority < pri) {
-        dir = DIRECTION_EAST;
-        priority = pri;
-      }
-    }
-    // 南
-    if ((walls_[pos_.y][pos_.x].south & mask) == WALL_NOT_EXISTS &&
-        pos_.y - 1 > -1) {
-      int pri = getPriority(pos_.x, pos_.y - 1, DIRECTION_SOUTH);
-      auto step = steps_[pos_.y - 1][pos_.x];
-      if (step < minStep) {
-        minStep = step;
-        dir = DIRECTION_SOUTH;
-        priority = pri;
-      } else if (step == minStep && priority < pri) {
-        dir = DIRECTION_SOUTH;
-        priority = pri;
-      }
-    }
-    // 西
-    if ((walls_[pos_.y][pos_.x].west & mask) == WALL_NOT_EXISTS &&
-        pos_.x - 1 > -1) {
-      int pri = getPriority(pos_.x - 1, pos_.y, DIRECTION_WEST);
-      auto step = steps_[pos_.y][pos_.x - 1];
-      if (step < minStep) {
-        minStep = step;
-        dir = DIRECTION_WEST;
-        priority = pri;
-      } else if (step == minStep && priority < pri) {
-        dir = DIRECTION_WEST;
-        priority = pri;
-      }
-    }
-
-    return dir;
-  }
+  Direction getNextDir();
 
   // 迷路を標準出力する
-  void print() {
-    constexpr auto MAZE_VERT_INDEX_PADDING = "    ";
-    constexpr auto MAZE_VERT_INDEX_FORMAT = " %2d ";
-    constexpr auto MAZE_HORIZ_INDEX_PADDING = "    ";
-    constexpr auto MAZE_HORIZ_INDEX_FORMAT = " %2d ";
-
-    constexpr auto WALL_COLOR_NOT_EXISTS = "\x1b[37m";
-    constexpr auto WALL_COLOR_EXISTS = "\x1b[37m";
-    constexpr auto WALL_COLOR_VIRTUAL = "\x1b[33m";
-    constexpr auto WALL_COLOR_UNKNOWN = "\x1b[31m";
-    constexpr auto WALL_COLOR_RESET = "\x1b[0m";
-
-    printf("%sNot Exists :     %s\n", WALL_COLOR_NOT_EXISTS, WALL_COLOR_RESET);
-    printf("%sExists     : --- %s\n", WALL_COLOR_EXISTS, WALL_COLOR_RESET);
-    printf("%sVirtual    : --- %s\n", WALL_COLOR_VIRTUAL, WALL_COLOR_RESET);
-    printf("%sNot Visited: --- %s\n", WALL_COLOR_UNKNOWN, WALL_COLOR_RESET);
-
-    for (auto y = MAZE_SIZE_Y - 1; y > -1; y--) {
-      printf("%s", MAZE_VERT_INDEX_PADDING);
-      // 北側の壁を出力
-      for (auto x = 0; x < MAZE_SIZE_X; x++) {
-        switch (walls_[y][x].north) {
-          case WALL_NOT_EXISTS:
-            printf("+%s   %s", WALL_COLOR_NOT_EXISTS, WALL_COLOR_RESET);
-            break;
-          case WALL_EXISTS:
-            printf("+%s---%s", WALL_COLOR_EXISTS, WALL_COLOR_RESET);
-            break;
-          case WALL_VIRTUAL:
-            printf("+%s---%s", WALL_COLOR_VIRTUAL, WALL_COLOR_RESET);
-            break;
-          case WALL_NOT_VISITED:
-            printf("+%s---%s", WALL_COLOR_UNKNOWN, WALL_COLOR_RESET);
-            break;
-        }
-      }
-      printf("+\n");
-
-      printf(MAZE_VERT_INDEX_FORMAT, y);
-      for (auto x = 0; x < MAZE_SIZE_X; x++) {
-        // 西側の壁を出力
-        switch (walls_[y][x].west) {
-          case WALL_NOT_EXISTS:
-            printf("%s %s", WALL_COLOR_NOT_EXISTS, WALL_COLOR_RESET);
-            break;
-          case WALL_EXISTS:
-            printf("%s|%s", WALL_COLOR_EXISTS, WALL_COLOR_RESET);
-            break;
-          case WALL_VIRTUAL:
-            printf("%s|%s", WALL_COLOR_VIRTUAL, WALL_COLOR_RESET);
-            break;
-          case WALL_NOT_VISITED:
-            printf("%s|%s", WALL_COLOR_UNKNOWN, WALL_COLOR_RESET);
-            break;
-        }
-        // 歩数を出力
-        if (pos_.x == x && pos_.y == y) {
-          printf(" x ");
-        } else {
-          printf("%3d", steps_[y][x]);
-        }
-      }
-
-      // 東側の壁を出力
-      switch (walls_[y][MAZE_SIZE_X - 1].east) {
-        case WALL_NOT_EXISTS:
-          printf("%s %s", WALL_COLOR_NOT_EXISTS, WALL_COLOR_RESET);
-          break;
-        case WALL_EXISTS:
-          printf("%s|%s", WALL_COLOR_EXISTS, WALL_COLOR_RESET);
-          break;
-        case WALL_VIRTUAL:
-          printf("%s|%s", WALL_COLOR_VIRTUAL, WALL_COLOR_RESET);
-          break;
-        case WALL_NOT_VISITED:
-          printf("%s|%s", WALL_COLOR_UNKNOWN, WALL_COLOR_RESET);
-          break;
-      }
-      printf("\n");
-    }
-
-    // 南側の壁を出力
-    printf("%s", MAZE_HORIZ_INDEX_PADDING);
-    for (auto x = 0; x < MAZE_SIZE_X; x++) {
-      switch (walls_[0][x].south) {
-        case WALL_NOT_EXISTS:
-          printf("+%s   %s", WALL_COLOR_NOT_EXISTS, WALL_COLOR_RESET);
-          break;
-        case WALL_EXISTS:
-          printf("+%s---%s", WALL_COLOR_EXISTS, WALL_COLOR_RESET);
-          break;
-        case WALL_VIRTUAL:
-          printf("+%s---%s", WALL_COLOR_VIRTUAL, WALL_COLOR_RESET);
-          break;
-        case WALL_NOT_VISITED:
-          printf("+%s---%s", WALL_COLOR_UNKNOWN, WALL_COLOR_RESET);
-          break;
-      }
-    }
-    printf("+\n");
-
-    // x座標目盛りを出力
-    printf("%s", MAZE_HORIZ_INDEX_PADDING);
-    for (auto x = 0; x < MAZE_SIZE_X; x++) {
-      printf(MAZE_HORIZ_INDEX_FORMAT, x);
-    }
-    printf("\n");
-  }
+  void print();
 
  private:
+  // 歩数を保持する2次元配列
   std::array<std::array<uint8_t, MAZE_SIZE_X>, MAZE_SIZE_Y> steps_;
+  // 壁の有無、訪問済みかを保持する2次元配列
   std::array<std::array<Walls, MAZE_SIZE_X>, MAZE_SIZE_Y> walls_;
 
   std::queue<Coord> updateQueue_;
@@ -441,8 +148,8 @@ class Map {
   // 指定座標が未探索かどうかを取得
   bool isNotVisited(int x, int y) {
     const auto &walls = walls_[y][x];
-    return walls.north == WALL_NOT_VISITED || walls.east == WALL_NOT_VISITED ||
-           walls.south == WALL_NOT_VISITED || walls.west == WALL_NOT_VISITED;
+    return !(walls.stepped.north && walls.stepped.east && walls.stepped.south &&
+             walls.stepped.west);
   }
 
   // 進行方向の優先度を取得する
@@ -464,4 +171,10 @@ class Map {
 
     return priority;
   }
+
+  // 自身の向きを標準出力する
+  static void printPos(Direction dir);
+
+  // 壁を標準出力する
+  static void printWall(Direction dir, Walls walls);
 };
--- test/maze/main.cc
+++ test/maze/main.cc
@@ -10,68 +10,54 @@ int main(int argc, char **argv) {
   int goal_x[MAZE_GOAL_SIZE_X] = {14, 15, 16},
       goal_y[MAZE_GOAL_SIZE_Y] = {14, 15, 16};
   Map map(goal_x, goal_y);
-  Map::Direction dir = Map::DIRECTION_NORTH;
 
+  // 自己位置を原点に設定
   map.setPos(0, 0);
 
-  printf("\x1b[2J");
-  printf("\x1b[0;0H");
-  map.print();
-
   Map::Walls walls;
-
   // 探索走行
   while (!map.inGoal(goal_x, goal_y)) {
     // 壁設定
-    uint8_t wallsSer = mazeData[map.getPos().y][map.getPos().x];
-    walls.north =
-        ((wallsSer >> 0) & 0x01) ? Map::WALL_EXISTS : Map::WALL_NOT_EXISTS;
-    walls.east =
-        ((wallsSer >> 1) & 0x01) ? Map::WALL_EXISTS : Map::WALL_NOT_EXISTS;
-    walls.south =
-        ((wallsSer >> 2) & 0x01) ? Map::WALL_EXISTS : Map::WALL_NOT_EXISTS;
-    walls.west =
-        ((wallsSer >> 3) & 0x01) ? Map::WALL_EXISTS : Map::WALL_NOT_EXISTS;
-
+    walls.byte.exist = mazeData[map.getPos().y][map.getPos().x];
+    // 壁を設定
     map.setWall(map.getPos().x, map.getPos().y, walls);
+    // 出力
+    printf("\x1b[0;0H");
+    map.print();
+    usleep(1000 * DELAY_MS);
     // 歩数マップを更新
     map.initStepsToGoal(goal_x, goal_y);
     map.makeSteps(false);
     // 次に進んで、自分の位置を更新
-    map.setPos(map.getNextDir(false));
-    // 出力
-    printf("\x1b[0;0H");
-    map.print();
-    usleep(1000 * DELAY_MS);
+    map.setPos(map.getNextDir());
   }
   // スタート座標まで戻る
   while (!map.inStart()) {
     // 壁設定
-    uint8_t wallsSer = mazeData[map.getPos().y][map.getPos().x];
-    walls.north =
-        ((wallsSer >> 0) & 0x01) ? Map::WALL_EXISTS : Map::WALL_NOT_EXISTS;
-    walls.east =
-        ((wallsSer >> 1) & 0x01) ? Map::WALL_EXISTS : Map::WALL_NOT_EXISTS;
-    walls.south =
-        ((wallsSer >> 2) & 0x01) ? Map::WALL_EXISTS : Map::WALL_NOT_EXISTS;
-    walls.west =
-        ((wallsSer >> 3) & 0x01) ? Map::WALL_EXISTS : Map::WALL_NOT_EXISTS;
-
+    walls.byte.exist = mazeData[map.getPos().y][map.getPos().x];
+    // 壁を設定
     map.setWall(map.getPos().x, map.getPos().y, walls);
-    // 歩数マップを更新
-    map.initStepsToStart();
-    map.makeSteps(false);
-    // 次に進んで、自分の位置を更新
-    map.setPos(map.getNextDir(false));
     // 出力
     printf("\x1b[0;0H");
     map.print();
     usleep(1000 * DELAY_MS);
+    // 歩数マップを更新
+    map.initStepsToStart();
+    map.makeSteps(false);
+    // 次に進んで、自分の位置を更新
+    map.setPos(map.getNextDir());
   }
+  map.rotateDir();
   // 最短走行
-  map.initStepsToGoal(goal_x, goal_y);
-  map.makeSteps(true);
-  // 出力
-  printf("\x1b[0;0H");
-  map.print();
+  while (!map.inGoal(goal_x, goal_y)) {
+    // 出力
+    printf("\x1b[0;0H");
+    map.print();
+    usleep(1000 * DELAY_MS);
+    // 歩数マップを更新
+    map.initStepsToGoal(goal_x, goal_y);
+    map.makeSteps(true);
+    // 次に進んで、自分の位置を更新
+    map.setPos(map.getNextDir());
+  }
 }
