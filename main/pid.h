b602cad3ab400b64bff5bfa83f41aae61f4b2cdb
ypc2e55orj <ypc2e55orj@gmail.com>
2024-02-16 18:31:41 +0900

    整理

--- main/pid.h
+++ main/pid.h
@@ -4,12 +4,9 @@
 
 class Pid {
  private:
-  struct Gain {
-    struct {
-      bool enable;
-      float value;
-    } kp, ki, kd;
-  } gain_{};
+  float gain_kp_{};
+  float gain_ki_{};
+  float gain_kd_{};
 
   float prev_target_{};
   float prev_{};
@@ -19,16 +16,11 @@ class Pid {
 
  public:
   explicit Pid(float kp, float ki, float kd) {
-    gain_.kp.enable = true;
-    gain_.ki.enable = true;
-    gain_.kd.enable = true;
-
-    gain_.kp.value = kp;
-    gain_.ki.value = ki;
-    gain_.kd.value = kd;
+    gain_kp_ = kp;
+    gain_ki_ = ki;
+    gain_kd_ = kd;
     reset();
   }
-  Gain &gain() { return gain_; }
 
   void reset() {
     prev_target_ = 0.0f;
@@ -40,27 +32,18 @@ class Pid {
   float update(float target, float current, float dt) {
     float ret = 0.0f;
 
-    if (gain_.kp.enable) {
-      ret += gain_.kp.value * (target - current);
-    }
-    if (gain_.ki.enable) {
-      ret += gain_.ki.value * (sum_target_ - sum_);
-    }
-    if (gain_.kd.enable) {
-      ret -= gain_.kd.value * (prev_target_ - prev_) / dt;
-    }
+    ret = gain_kp_ * (target - current) + gain_ki_ * (sum_target_ - sum_) - gain_kd_ * (prev_target_ - prev_) / dt;
+
+    prev_target_ = target;
+    prev_ = current;
 
     if ((!std::signbit(target) && (sum_target_ + target) > sum_target_) ||
         (std::signbit(target) && (sum_target_ + target) < sum_target_)) {
       sum_target_ += target;
     }
-
     if ((!std::signbit(current) && (sum_ + current) > sum_) || (std::signbit(current) && (sum_ + current) < sum_)) {
       sum_ += current;
     }
-
-    prev_target_ = target;
-    prev_ = current;
 
     return ret;
   }
